bdde7716a53df3cca9a9725bf94656cafaf14881
3054788988 <3054788988@qq.com>
2016-03-12 15:33:30 +0800

    13081260

---
+++ canvas_oop/canvas_oop.ts
@@ -0,0 +1,137 @@
+/**
+ * 基类，负责处理x,y,rotation 等属性
+ */ 
+class DisplayObject {
+
+    x = 0;
+
+    y = 0;
+
+    rotation = 0;
+
+    draw(context: CanvasRenderingContext2D) {
+        context.save();
+        context.rotate(this.rotation);
+        context.translate(this.x, this.y);
+        this.render(context);
+
+        context.restore();
+    }
+
+    render(context: CanvasRenderingContext2D) {
+
+    }
+
+}
+
+class Bitmap extends DisplayObject {
+
+
+    source;
+
+    render(context: CanvasRenderingContext2D) {
+
+        var image = imagePool[this.source];
+        if (image) {
+            context.drawImage(image, 0, 0);
+        }
+        else {
+            context.font = "20px Arial";
+            context.fillStyle = '#000000';
+            context.fillText('错误的URL', 0, 20);
+        }
+    }
+
+}
+
+class Rect extends DisplayObject {
+
+    width = 100
+
+    height = 100;
+
+    color = '#FF0000';
+
+    render(context: CanvasRenderingContext2D) {
+        context.fillStyle = this.color;
+        context.fillRect(0, 0, this.width, this.height);
+    }
+}
+
+class TextField extends DisplayObject {
+
+    render(context: CanvasRenderingContext2D) {
+        context.font = "20px Arial";
+        context.fillStyle = '#000000';
+        context.fillText('账号', 0, 20);
+    }
+}
+
+function drawQueue(queue) {
+    for (var i = 0; i < renderQueue.length; i++) {
+        var displayObject: DisplayObject = renderQueue[i];
+        displayObject.draw(context);
+    }
+}
+
+var imagePool = {};
+
+function loadResource(imageList, callback) {
+    var count = 0;
+    imageList.forEach(function(imageUrl) {
+        var image = new Image();
+        image.src = imageUrl;
+        image.onload = onLoadComplete;
+        image.onerror = onLoadError;
+
+        function onLoadComplete() {
+            imagePool[imageUrl] = image;
+            count++;
+            if (count == imageList.length) {
+                callback();
+            }
+        }
+        
+        function onLoadError(){
+            alert('资源加载失败:' + imageUrl);
+        }
+    })
+}
+
+
+var canvas: HTMLCanvasElement = document.getElementById("game") as HTMLCanvasElement;
+var context = canvas.getContext("2d");
+
+
+
+
+var text = new TextField();
+text.x = 100;
+
+
+
+var bitmap1= new Bitmap();
+bitmap1.source = 'ｂｇ.jpg';
+
+var bitmap2= new Bitmap();
+bitmap2.source = '１１７７３.png';
+
+var bitmap3= new Bitmap();
+bitmap3.source = 'ｔｏｕｘｉａｎｇ.png';
+
+var bitmap4= new Bitmap();
+bitmap4.source = 'ｋａｉｓｈｉ.png';
+
+
+//渲染队列
+var renderQueue = [ bitmap1,bitmap2,bitmap3,bitmap4];
+
+//资源加载列表
+var imageList = ['ｂｇ.jpg','１１７７３.png','ｔｏｕｘｉａｎｇ.png','ｋａｉｓｈｉ.png'];
+
+//先加载资源，加载成功之后执行渲染队列
+loadResource(imageList, function() {
+    drawQueue(renderQueue);
+})
+
+
